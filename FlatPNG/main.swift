c91aaf979cf3deee0ab9b4362b6be54e965e0112
Maxim Ananov <pointum@gmail.com>
2015-09-29 12:46:24 +0300

    Initial Commit

---
+++ FlatPNG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FlatPNG
+//
+//  Created by Maxim Ananov on 9/29/15.
+//  Copyright © 2015 Maxim Ananov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
